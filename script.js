95148c796ed68d8b800ec0673ef6507b54274b6e
VIkash Kumar <103258656+vikash-mdh@users.noreply.github.com>
2025-02-08 18:50:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+const line = document.getElementById('line');
+let rotation = 0;
+ 
+setInterval(() => {
+  rotation += 5;
+  line.style.transform = `translate(-50%, -50%) rotate(${rotation}deg)`;
+}, 50);
